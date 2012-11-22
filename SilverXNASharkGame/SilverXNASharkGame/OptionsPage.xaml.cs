058ef8ebe8af80f99159968047456b65a3cbf92c
aleksandersmierciak <aleksander.smierciak+github@gmail.com>
2012-11-22 19:09:31 +0200

    Finished transforming to Silverlight + XNA project, fixed some movement-related bugs, attempted making sand particles more realistic looking

---
+++ SilverXNASharkGame/SilverXNASharkGame/OptionsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace SilverXNASharkGame
+{
+    using Microsoft.Phone.Controls;
+
+    /// <summary>
+    /// Represents a page containing all game options, accessible from the main game menu.
+    /// </summary>
+    public partial class OptionsPage : PhoneApplicationPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionsPage" /> class.
+        /// </summary>
+        public OptionsPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
